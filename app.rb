56b53b77045acf28492d4c288326193ca9347e37
reneew08 <rwu30@chicagobooth.edu>
2024-10-24 02:28:08 +0000

    complete

--- app.rb
+++ app.rb
@@ -5,14 +5,16 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_add = params.fetch("first_add").to_f
+  @second_add = params.fetch("second_add").to_f
 
-  @result = @first_num + @second_num
+
+  @add_result = @first_add + @second_add
   erb(:add_result)
 end
 
@@ -21,17 +23,20 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
-
+  @first_sub = params.fetch("first_sub").to_f
+  @second_sub = params.fetch("second_sub").to_f
+  @sub_result=@second_sub-@first_sub
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_mult = params.fetch("first_mult").to_f
+  @second_mult = params.fetch("second_mult").to_f
+  @mult_result=@first_mult *@second_mult
   erb(:mult_result)
 end
 
@@ -40,5 +45,8 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_div= params.fetch("first_div").to_f
+  @second_div = params.fetch("second_div").to_f
+  @div_result=@first_div/@second_div
   erb(:div_result)
 end
